b365cbf9bcbf095694d2b6511d66a27a909abd5f
Егор Москалев <moskalevegor310@gmail.com>
2025-12-02 14:22:57 +0500

    Task1

--- Tyuiu.MoskalevEA.Sprint4.Task1.V16.Lib/DataService.cs
+++ Tyuiu.MoskalevEA.Sprint4.Task1.V16.Lib/DataService.cs
@@ -4,18 +4,21 @@ namespace Tyuiu.MoskalevEA.Sprint4.Task1.V16.Lib
 {
     public class DataService : ISprint4Task1V16
     {
+        
         public int Calculate(int[] array)
         {
-             int sum = 0;
-             foreach (int i in array)
-             {
-                 if (i % 2 != 0)
-                 {
-                    sum += i;
-                 }
-             }
-             return sum;
-            
+            int sum = 1;
+
+            foreach (int element in array)
+            {
+                if (element % 2 != 0)
+                {
+                    sum *= element;
+                }
+            }
+            return sum;
         }
     }
+
 }
+
--- Tyuiu.MoskalevEA.Sprint4.Task1.V16/Program.cs
+++ Tyuiu.MoskalevEA.Sprint4.Task1.V16/Program.cs
@@ -15,8 +15,25 @@ class Program
         Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
         Console.WriteLine("***************************************************************************");
 
-        int[] array = new int[14];
-        Console.WriteLine("Введите 14 целых чисел в диапазоне от 3 до 8:");
+        int[] array = new int[13];
+        Console.WriteLine("Введите 13 целых чисел в диапазоне от 3 до 8:");
+
+        for (int i = 0; i < 13; i++)
+        {
+            Console.Write($"Элемент [{i + 1}]: ");
+            string input = Console.ReadLine();
+            int value = int.Parse(input);
+
+
+            if (value < 3 || value > 8)
+            {
+                Console.WriteLine("Вне задания");
+                i--;
+                continue;
+            }
+
+            array[i] = value;
+        }
 
         Console.WriteLine("Массив:");
         for (int i = 0; i < array.Length; i++)
@@ -32,8 +49,9 @@ class Program
 
         int sum = ds.Calculate(array);
 
-        Console.WriteLine("Сумма нечётных элементов = " + sum);
+        Console.WriteLine("Сумма чётных элементов = " + sum);
 
         Console.ReadKey();
     }
 }
+
