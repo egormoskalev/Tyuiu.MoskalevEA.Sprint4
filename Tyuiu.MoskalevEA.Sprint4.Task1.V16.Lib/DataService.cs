f8b1117c4ac5455ea48236425578e28b42a85d92
Егор Москалев <moskalevegor310@gmail.com>
2025-12-02 12:48:29 +0500

    Task1

---
+++ Tyuiu.MoskalevEA.Sprint4.Task1.V16.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MoskalevEA.Sprint4.Task1.V16.Lib
+{
+    public class DataService : ISprint4Task1V16
+    {
+        public int Calculate(int[] array)
+        {
+             int sum = 0;
+             foreach (int i in array)
+             {
+                 if (i % 2 != 0)
+                 {
+                    sum += i;
+                 }
+             }
+             return sum;
+            
+        }
+    }
+}
---
+++ Tyuiu.MoskalevEA.Sprint4.Task1.V16/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.MoskalevEA.Sprint4.Task1.V16.Lib;
+
+namespace Tyuiu.MoskalevEA.Sprint4.Task1.V16;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт  #4                                                              *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #16                                                             *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int[] array = new int[14];
+        Console.WriteLine("Введите 14 целых чисел в диапазоне от 3 до 8:");
+
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+            if (i < array.Length - 1) Console.Write(", ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int sum = ds.Calculate(array);
+
+        Console.WriteLine("Сумма нечётных элементов = " + sum);
+
+        Console.ReadKey();
+    }
+}
