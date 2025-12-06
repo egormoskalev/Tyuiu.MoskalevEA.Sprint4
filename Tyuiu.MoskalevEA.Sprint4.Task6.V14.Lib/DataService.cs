83f235dc26fec0f85b57f4920a135c021319de6a
Егор Москалев <moskalevegor310@gmail.com>
2025-12-07 04:37:01 +0500

    Task6

---
+++ Tyuiu.MoskalevEA.Sprint4.Task6.V14.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MoskalevEA.Sprint4.Task6.V14.Lib
+{
+    public class DataService : ISprint4Task6V5
+    {
+        public int Calculate(string[] array)
+        {
+            string[] filtered = Array.FindAll(array, s => s.Length == 3);
+            return filtered.Length;
+        }
+    }
+}
---
+++ Tyuiu.MoskalevEA.Sprint4.Task6.V14.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MoskalevEA.Sprint4.Task6.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
