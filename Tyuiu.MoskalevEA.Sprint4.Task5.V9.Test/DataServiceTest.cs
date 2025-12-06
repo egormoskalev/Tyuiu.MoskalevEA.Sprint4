1f1d35e5e45c32ba08a8f33a98d7bddd36c550d2
Егор Москалев <moskalevegor310@gmail.com>
2025-12-07 04:25:18 +0500

    Task5

--- Tyuiu.MoskalevEA.Sprint4.Task5.V9.Test/DataServiceTest.cs
+++ Tyuiu.MoskalevEA.Sprint4.Task5.V9.Test/DataServiceTest.cs
@@ -1,11 +1,28 @@
-﻿namespace Tyuiu.MoskalevEA.Sprint4.Task5.V9.Test
+﻿using Tyuiu.MoskalevEA.Sprint4.Task5.V9.Lib;
+
+namespace Tyuiu.MoskalevEA.Sprint4.Task5.V9.Test
+
 {
     [TestClass]
-    public sealed class DataServiceTest
+    public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidCalculate()
         {
+            DataService ds = new DataService();
+
+            int[,] array = {
+                {-4, -4, -1, -2, 2},
+                {0, 2, -3, 4, 4},
+                {-3, 2, -2, 1, 3},
+                {4, 1, -4, 2, -4},
+                {4, 1, 1, -1, 2}
+            };
+
+            int result = ds.Calculate(array);
+            int expected = 33;
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
