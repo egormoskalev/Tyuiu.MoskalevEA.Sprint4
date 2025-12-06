897d77ee83b4f213b26f3f05b40e0138b52c26eb
Егор Москалев <moskalevegor310@gmail.com>
2025-12-06 15:48:58 +0500

    Task3

---
+++ Tyuiu.MoskalevEA.Sprint4.Task3.V22.lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MoskalevEA.Sprint4.Task3.V22.Lib
+{
+    public class DataService : ISprint4Task3V10
+    {
+        public int Calculate(int[,] array)
+        {
+            int max = array[4, 4];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(0); j++)
+                {
+                    if (array[i, j] >= max)
+                        max = array[i, j];    
+                }
+            }
+        return max;
+        }
+    }
+}
