737624b6cdd8197554e42762dd9aba76fe2b39fa
Егор Москалев <moskalevegor310@gmail.com>
2025-12-06 14:53:52 +0500

    Task2

---
+++ Tyuiu.MoskalevEA.Sprint4.Task2.V29.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MoskalevEA.Sprint4.Task2.V29.Lib
+{
+    public class DataService : ISprint4Task2V29
+    {
+        public int Calculate(int[] array)
+        {
+            int sum = 1;
+
+            foreach (int element in array)
+            {
+                if (element % 2 == 0)
+                {
+                    sum *= element;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.MoskalevEA.Sprint4.Task2.V29/Program.cs
@@ -0,0 +1,56 @@
+﻿using Tyuiu.MoskalevEA.Sprint4.Task2.V29.Lib;
+
+namespace Tyuiu.MoskalevEA.Sprint4.Task2.V29;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт  #4                                                              *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #16                                                             *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int[] array = new int[11];
+        Console.WriteLine("Введите 13 целых чисел в диапазоне от 1 до 8:");
+
+        for (int i = 0; i < 11; i++)
+        {
+            Console.Write($"Элемент [{i + 1}]: ");
+            string input = Console.ReadLine();
+            int value = int.Parse(input);
+
+
+            if (value < 1 || value > 8)
+            {
+                Console.WriteLine("Вне задания");
+                i--;
+                continue;
+            }
+
+            array[i] = value;
+        }
+
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+            if (i < array.Length - 1) Console.Write(", ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int sum = ds.Calculate(array);
+
+        Console.WriteLine("Сумма чётных элементов = " + sum);
+
+        Console.ReadKey();
+    }
+}
