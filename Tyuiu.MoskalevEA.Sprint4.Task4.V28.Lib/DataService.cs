dc6cc0054f60ebe0260ba1d4b9c2b6043a016ee8
Егор Москалев <moskalevegor310@gmail.com>
2025-12-07 03:54:56 +0500

    Task4

---
+++ Tyuiu.MoskalevEA.Sprint4.Task4.V28.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MoskalevEA.Sprint4.Task4.V28.Lib
+{
+    public class DataService : ISprint4Task3V28
+    {
+        public int[,] Calculate(int[,] array)
+        {
+            int rows = array.GetLength(0);
+            int cols = array.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (array[i, j] % 2 == 0)
+                        array[i, j] = 1;
+                }
+            }
+
+            return array;
+        }
+
+        int ISprint4Task3V28.Calculate(int[,] array)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tyuiu.MoskalevEA.Sprint4.Task4.V28/Program.cs
@@ -0,0 +1,63 @@
+﻿using Tyuiu.MoskalevEA.Sprint4.Task4.V28.Lib;
+
+namespace Tyuiu.MoskalevEA.Sprint4.Task4.V28;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт  #4                                                              *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #28                                                             *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int[,] array = new int[5, 5];
+        Console.WriteLine("Введите 25 целых чисел в диапазоне от 2 до 8:");
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write($"Элемент [{i + 1},{j + 1}]: ");
+                string input = Console.ReadLine();
+                int value = int.Parse(input);
+
+                array[i, j] = value;
+            }
+        }
+
+        Console.WriteLine("Исходный массив:");
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write(array[i, j]);
+                if (j < 4) Console.Write(", ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int[,] modifiedArray = ds.Calculate(array);
+
+        Console.WriteLine("Массив после замены чётных на 1:");
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write(modifiedArray[i, j]);
+                if (j < 4) Console.Write(", ");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+}
+
