d10898a7e68e4ae2ba0ac23e5e3897949144893b
Егор Москалев <moskalevegor310@gmail.com>
2025-12-07 05:36:24 +0500

    Task7

---
+++ Tyuiu.MoskalevEA.Sprint4.Task7.V21.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MoskalevEA.Sprint4.Task7.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.MoskalevEA.Sprint4.Task7.V21/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.MoksalevEA.Sprint4.Task7.V21.Lib;
+
+namespace Tyuiu.MoksalevEA.Sprint4.Task7.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int n = 3;
+            int m = 4;
+            int[,] mtrx = new int[n, m];
+            string str = "425963128528";
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #4                                                              *");
+            Console.WriteLine("* Задание #7                                                             *");
+            Console.WriteLine("* Вариант #21                                                            *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int index = 0;
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                               *");
+            Console.WriteLine("************************************************************");
+
+            int res = ds.Calculate(n, m, str);
+            Console.WriteLine("Сумма нечётных чисел = " + res);
+            Console.ReadKey();
+        }
+    }
+}
